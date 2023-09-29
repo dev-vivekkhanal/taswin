0d28cd0efd7daff92a3ab80dde36b9bcf9852dcf
Aayanjit <aayanjit@ekoinfomatics.com>
2023-09-29 18:43:18 +0530

    Added glass morphism effect

--- src/components/landing/Section7.jsx
+++ src/components/landing/Section7.jsx
@@ -10,13 +10,13 @@ const Section7 = () => {
     return (
         <div className='w-full pb-32'>
             <div className='w-full hidden md:block'>
-                <div className='w-[65%] lg:w-[60%] mx-auto rounded-[30px] py-20 px-2 bg-[#f5f5f5d3] shadow-lg relative p-5 md:translate-y-[11rem] z-50'>
+                <div className='w-[65%] lg:w-[60%] mx-auto rounded-[30px] py-20 px-2 glass-morphism shadow-lg relative p-5 md:translate-y-[11rem] z-50'>
                     <div className='w-full max-w-[420px] ml-8 mr-2 lg:ml-20 xl:ml-32'>
                         <h1 className='font-[700] text-[28px]'>Join our newsletter now!</h1>
                         <p className='text-[14px]'>Register now and get our latest updates and promos.</p>
                         <div className='w-full pt-10'>
                             <div className='w-full flex'>
-                                <input type="text" className='w-full px-3 pl-8 text-[14px] py-2 rounded-l-[5px]' placeholder='Enter your email' />
+                                <input type="text" className='w-full px-3 border border-[#a7a7a75b] pl-8 text-[14px] py-2 rounded-l-[5px]' placeholder='Enter your email' />
                                 <button className='shadow-md transition-all active:scale-95 text-[13px] text-white flex justify-center items-center -translate-x-[2px] px-8 py-2 bg-[#07260E] rounded-[5px]'>Join</button>
                             </div>
                         </div>
@@ -30,13 +30,13 @@ const Section7 = () => {
             <div className='block md:hidden'>
                 <div className=' md:translate-x-12 relative'>
                     <div className='absolute inset-0 flex justify-center items-center'>
-                    <div className='w-[80%] sm:w-[70%] rounded-[30px] py-20 bg-[#f5f5f5d3] shadow-lg p-5 md:translate-y-[11rem] relative z-50'>
+                    <div className='w-[80%] sm:w-[70%] rounded-[30px] py-20 glass-morphism shadow-lg p-5 md:translate-y-[11rem] relative z-50'>
                         <div className='w-full max-w-[420px] flex flex-col items-center'>
                             <h1 className='font-[700] text-[28px]'>Join our newsletter now!</h1>
                             <p className='text-[14px]'>Register now and get our latest updates and promos.</p>
                             <div className='w-full pt-10'>
                                 <div className='w-full flex'>
-                                    <input type="text" className='w-full px-3 pl-4 md:pl-8 text-[14px] py-2 rounded-l-[5px]' placeholder='Enter your email' />
+                                    <input type="text" className='w-full border border-[#a7a7a75b] px-3 pl-4 md:pl-8 text-[14px] py-2 rounded-l-[5px]' placeholder='Enter your email' />
                                     <button className='text-[13px] text-white flex justify-center items-center -translate-x-[2px] px-8 py-2 bg-[#07260E] rounded-[5px]'>Join</button>
                                 </div>
                             </div>
@@ -46,6 +46,8 @@ const Section7 = () => {
                     <img src={img_mobile} className='block md:hidden w-full z-10' alt="" />
                 </div>
             </div>
+
+
             <div className='w-full text-center pt-32 lg:pt-24'>
                 <div className='w-[80%] lg:w-[60%] xl:w-[50%] mx-auto flex flex-col items-center gap-8'>
                     <h1 className='text-[28px] font-[700]'>Progress on the farm requires a steady stream of human ingenuity.</h1>
