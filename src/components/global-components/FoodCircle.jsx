f72086cbb1a4e6c1ddc64341eadfc16ef1608810
Vivek <dev.vivekkhanal@gmail.com>
2024-01-13 22:38:37 +0530

    fixed vegtable responsive

--- src/components/global-components/FoodCircle.jsx
+++ src/components/global-components/FoodCircle.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import food from '../../assets/landing/foodcircle.webp'
+import React from "react";
+import food from "../../assets/landing/foodcircle.webp";
 
 const FoodCircle = () => {
-    return (
-        <div className="absolute w-full flex inset-0 overflow-hidden z-[10]">
-            <div className="w-full max-w-[95vw] flex items-center -tanslate-y-[200px] justify-between ">
-                <div className="w-full max-w-[1000px] -translate-x-[500px]">
-                    <img src={food} alt="" className=" animation-spin" />
-                </div>
-                <div className="w-full max-w-[1000px] translate-x-[500px]">
-                    <img src={food} alt="" className=" animation-spin" />
-                </div>
-            </div>
+  return (
+    <div className="absolute w-full inset-0 overflow-hidden z-[10] hidden lg:flex">
+      <div className="w-full  flex items-center justify-between overflow-hidden ">
+        <div className="w-full max-w-[30%] -translate-x-[60%]">
+          <img src={food} alt="" className=" animation-spin" />
         </div>
-    )
-}
+        <div className="w-full max-w-[30%] translate-x-[60%]">
+          <img src={food} alt="" className=" animation-spin" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default FoodCircle
+export default FoodCircle;
--- src/components/landing/Section2.jsx
+++ src/components/landing/Section2.jsx
@@ -8,7 +8,7 @@ import FoodCircle from "../global-components/FoodCircle";
 const Section2 = () => {
   return (
     <section className="py-32 relative">
-      <div className="w-[80%] mx-auto space-y-10 relative z-[100]">
+      <div className="w-[70%] mx-auto space-y-10 relative z-[100]">
         <img src={rice} alt="rice icon" />
         <div className="flex flex-col md:flex-row justify-center gap-32">
           <h1 className="font-bold text-4xl flex-1 text-primary">
@@ -17,13 +17,20 @@ const Section2 = () => {
           </h1>
 
           <p className="flex-1">
-            Celebrate nature's purity in our organic oasis, where the earth thrives naturally, untouched by harmful chemicals. Our nutrient-rich produce speaks volumes about our dedication to quality and sustainability. Immerse yourself in the richness of our chemical-free, nutritious harvests and relish the unparalleled flavors of nature's bounty, all while contributing to a healthier, greener planet.
+            Celebrate nature's purity in our organic oasis, where the earth
+            thrives naturally, untouched by harmful chemicals. Our nutrient-rich
+            produce speaks volumes about our dedication to quality and
+            sustainability. Immerse yourself in the richness of our
+            chemical-free, nutritious harvests and relish the unparalleled
+            flavors of nature's bounty, all while contributing to a healthier,
+            greener planet.
           </p>
         </div>
 
         <div className="flex flex-col md:flex-row justify-center  gap-32">
           <p className="flex-1">
-            As a company, we bridge the gap from farmers' fields to your table, delivering pure organic goodness.
+            As a company, we bridge the gap from farmers' fields to your table,
+            delivering pure organic goodness.
           </p>
           <div className="flex-1 flex gap-5 justify-center items-center">
             <div className="flex-1 bg-secondary rounded-[7px]">
